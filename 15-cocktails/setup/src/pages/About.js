3f50a70f2f649467799f0bb1dcbe44b09c1cd7ec
Siowyx <yeexian.siow@yale.edu>
2023-06-29 23:03:21 -0400

    completed project 15

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda est
+        suscipit voluptatem autem maiores nisi sapiente, amet perspiciatis
+        commodi officiis eum magnam magni dolores quae atque necessitatibus
+        incidunt ratione rerum quis. Deleniti vitae rerum ut neque saepe
+        dignissimos suscipit exercitationem!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
